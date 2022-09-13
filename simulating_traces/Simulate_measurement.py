34f78ff591a127fb9a0e3998ab0f489dbd00f267
Virginia Ciriano <viginia.ciriano@gmail.com>
2022-09-13 14:17:33 +0100

    Add max simulated histogram

--- simulating_traces/Simulate_measurement.py
+++ simulating_traces/Simulate_measurement.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def simulate_measurement(A, t_out, t_in, E_low, sigma_low , E_high, sigma_high,	 n_traces=1, t_max=2000, acq_rate=1):
+def simulate_measurement(A, t_out, t_in, E_low, sigma_low , E_high, sigma_high,	 n_traces=1, t_tot=2000, acq_rate=1):
 	''' Creates measurement traces using the input parameters and returns the maximum of each trace
 	  Parameters
 	  ----------
@@ -32,7 +32,7 @@ def simulate_measurement(A, t_out, t_in, E_low, sigma_low , E_high, sigma_high,	
 		  n_traces: integer
 			  Number of simulated traces	 
 
-		  t_max: float
+		  t_tot: float
 			Time during which data is acquire (in  mu s)
 			
 		  acq_rate:
@@ -48,15 +48,15 @@ def simulate_measurement(A, t_out, t_in, E_low, sigma_low , E_high, sigma_high,	
 	# and the right values (positive or null)
 	if t_out<0 or t_in<0:
 		raise ValueError("t_in and t_out should be positive numbers.")
-	if t_max*acq_rate< 3:
-		raise ValueError("The readout trace is too short. Try to increase the acquisition rate or the readout time (t_max).")
-	if t_max< 0 or acq_rate< 0: 
+	if t_tot*acq_rate< 3:
+		raise ValueError("The readout trace is too short. Try to increase the acquisition rate or the readout time (t_tot).")
+	if t_tot< 0 or acq_rate< 0: 
 		raise ValueError("The readout time and acquisition rate must be positive numbers.")
 
 
 	spin_traces = []
 	spin_number=np.empty(0)
-	n_points_time=int(t_max*acq_rate)
+	n_points_time=int(t_tot*acq_rate)
 	
 	for i in range(n_traces):
 		choose_spin = np.random.binomial(1,A,1) #create 1 with proba A and 0 with proba (1-A)
@@ -70,14 +70,14 @@ def simulate_measurement(A, t_out, t_in, E_low, sigma_low , E_high, sigma_high,	
 			
 		if choose_spin == 0 : #spin up
 		# create a spin up trace i.e. trace with a blip of E_high 
-			line_spin_up =spin_up_trace_sim(t_out, t_in, E_low, sigma_low, E_high, sigma_high, t_max, acq_rate)		  
+			line_spin_up =spin_up_trace_sim(t_out, t_in, E_low, sigma_low, E_high, sigma_high, t_tot, acq_rate)		  
 			spin_traces .append( line_spin_up )
 			
 	spin_traces =np.array(spin_traces )
-	time=np.arange(0,t_max, 1/acq_rate)		# sweep time over trace
+	time=np.arange(0,t_tot, 1/acq_rate)		# sweep time over trace
 	return(time,spin_traces,spin_number)	 # spin_traces : array of 1D traces, spin_number: array with their corresponding spin
 
-def spin_up_trace_sim(t_out, t_in, E_low, sigma_low, E_high, sigma_high, t_max, acq_rate):
+def spin_up_trace_sim(t_out, t_in, E_low, sigma_low, E_high, sigma_high, t_tot, acq_rate):
 	''' Creates a spin up measurement trace
 	  Parameters
 	  ----------
@@ -103,14 +103,14 @@ def spin_up_trace_sim(t_out, t_in, E_low, sigma_low, E_high, sigma_high, t_max, 
 			  noise during the blip		  
 	  
 	  User parameters
-		   t_max: float
+		   t_tot: float
 			Time during which data is acquire (in mu s)
 			
 		  acq_rate:
 			Acquisition rate in MHz	  
 			
 	'''	  
-	time=np.arange(0,t_max, 1/acq_rate) # sweep time over trace
+	time=np.arange(0,t_tot, 1/acq_rate) # sweep time over trace
 	spin_trace_up=np.zeros(len(time)) # spin-up trace
 	
 	t_leave=np.random.exponential(t_out) #random t_out time
@@ -133,7 +133,6 @@ def spin_up_trace_sim(t_out, t_in, E_low, sigma_low, E_high, sigma_high, t_max, 
 
 def moving_avg_filter(spin_traces,time,	 factor, acq_rate):
 	''' Applies moving average filter to the generated traces
-		and reduces the measurement bandwidth
 	  Parameters
 	  ----------
 		  spin_traces: 2D array
@@ -168,15 +167,15 @@ def moving_avg_filter(spin_traces,time,	 factor, acq_rate):
 		ret[factor:] = ret[factor:] - ret[:-factor] 
 		spin_traces_filtered.append(ret[factor - 1:] / factor)
 	spin_traces_filtered=np.array(spin_traces_filtered)
-	#I NEED TO CHANGE HOW TO CALCULATE THE MEAS_BW
-	meas_bw=1
+	
+	
 	
 	ret = np.cumsum(time, dtype=float)
 	ret[factor:] = ret[factor:] - ret[:-factor] 
 	time_filtered=(ret[factor - 1:] / factor)
 	time_filtered=time_filtered+factor/acq_rate/2 # Initial time is larger due to the filter
 	
-	return time_filtered, spin_traces_filtered, meas_bw
+	return time_filtered, spin_traces_filtered
  
   
  
@@ -184,95 +183,87 @@ def moving_avg_filter(spin_traces,time,	 factor, acq_rate):
 
 
 
-# We can put it as a class and there is a function called plot
-def max_histogram(spin_traces, bins=20, Plot=1):
-	return 
+
+def get_inter_bins(bins):
+	res = []
+	for i in range (len(bins)-1):
+		res.append( 1/2*(bins[i]+bins[i+1]))
+	return(res)
+
+def max_simulated_histogram(time, spin_traces, spin_number, t_min=0, t_max=None, num_bins=100, plot_histo=True):
+	from scipy.interpolate import interp1d
+	import matplotlib.pyplot as plt
+	''' Creates a histogram with the maximum of the spin traces
+	  Parameters
+	  ----------
+		  time: 1D array
+			Time sweep during a measurement
+		  spin_traces: 2D array
+			All data traces		  
+		  spin_number: 1D array
+			State of each data trace. 0 is spin up and 1 is spin down 
+		  t_min: float
+			start of the measurement trace
+		  t_max: float
+			end of the measurement trace
+		  bins:
+			 number of bins in the histogram
+		  Plot: bool
+			 True if the histogram needs to be visualised
+  '''
+	if t_max==None:
+		t_max=time.max()
+
+	minim= np.where(time>= t_min)[0][0]
+	maxim= np.where(time>= t_max)[0][0]
+	max_spin_traces=np.amax(spin_traces[:,minim:maxim],axis=1)
+	n_traces=len(spin_traces)
+	range_min=max_spin_traces.min()
+	range_max=max_spin_traces.max()
 
 
-def simulate_histogram_out(A,range_min,range_max,std_down,std_up, signal_low,signal_high,t_int,t_out,t_in,t_max,n_points_time,n_traces,num_bins = 60,plot_histo = 1): 
-    max_ss_up = []
-    max_ss_down = []
-    max_ss_bg = []
-    
-    minim= np.where(time>= t_min)[0][0]
-    maxim= np.where(time>= t.max())[0][0]
-    # Do many traces and get max
-    for i in range(n_traces):
-        
-        # Plot the traces for the first 5 traces 
-        #if i<5 :
-        #    ss_readout = simulate_ss_out(A,std_down,signal_low,signal_high,t_int,t_out,t_in,t_max,n_points_time, 1)
-
-        #else :
-        ss_readout = simulate_ss_out(A,std_down,std_up,signal_low,signal_high,t_int,t_out,t_in,t_max,n_points_time, 0)
-            
-        if ss_readout[2] == 1 : #choose_spin = 1 so spin is down
-            max_ss_down.append(ss_readout[0])
-            max_ss_bg.append(ss_readout[1])
-
-        if ss_readout[2] == 0 : #choose_spin = 0 so spin is up
-            max_ss_up.append(ss_readout[0])
-            max_ss_bg.append(ss_readout[1])
-        
-        #if i==np.int(n_traces/2):
-        #    print('Progress>50%')
-    
-    ma = np.append(np.array(max_ss_up),np.array(max_ss_down))
-    max_ss_bg = np.array(max_ss_bg)
+	# total histogram
+	hist_max_ss, bin_edges_max_ss = np.histogram(max_spin_traces,bins=num_bins,range = (range_min,range_max)) 
+	bin_axis_ss= bin_edges_max_ss[0:-1]+( bin_edges_max_ss[1]- bin_edges_max_ss[0])/2
+	normalisation=( bin_edges_max_ss[1]- bin_edges_max_ss[0])*n_traces # for histogram area =1	
 
 
-    hist_max_ss, bin_edges_max_ss = np.histogram(ma,bins=num_bins,range = (range_min,range_max))    
-    hist_max_ss_bg, bin_edges_max_ss_bg = np.histogram(max_ss_bg, bins=num_bins,range=(range_min,range_max) )
-    
-    bin_axis_ss= bin_edges_max_ss[0:-1]+( bin_edges_max_ss[1]- bin_edges_max_ss[0])/2
-    normalisation=( bin_edges_max_ss[1]- bin_edges_max_ss[0])*n_traces
-    
-    
-    
-    
-    for a in range(len(sim_distance_rem)):    
-        
+	# Spin up histogram
+	index_spin_up=np.where(spin_number==0)[0]
+	max_spin_up=max_spin_traces[index_spin_up]
+	hist_max_up, bin_edges_max_up = np.histogram(max_spin_up,bins=num_bins,range = (range_min,range_max)) 
+	
+	
+	# Spin down histogram
+	index_spin_down=np.where(spin_number==1)[0]
+	max_spin_down=max_spin_traces[index_spin_down]
+	hist_max_down, bin_edges_max_down = np.histogram(max_spin_up,bins=num_bins,range = (range_min,range_max)) 
+
+	if plot_histo:
+		# Plot the histogram 
+		plt.figure()
+		#plt.hist(max_spin_traces,bins=num_bins,label='total', alpha = 0.6,range = (range_min,range_max))
+		plt.hist(max_spin_up,bins=num_bins,label='spin up', alpha = 0.6,range = (range_min,range_max))
+		plt.hist(max_spin_down,bins=num_bins,label='spin down', alpha = 0.6,range = (range_min,range_max))
+		plt.xlabel("Signal maxima")
+		plt.ylabel("Probability")
+		plt.title('Histogram of the readout maxima')
+		#print('Parameters for the simulation :')
+		#print('T_int = %f us, T_in = %f us, T_out = %f us' %(t_int,t_in,t_out))
+		#print('A = %f, std = %f'%(A,std_down))
+		#plt.title('Simulated histogram as lines')
+		f_hist_inter = interp1d(get_inter_bins(bin_edges_max_ss), hist_max_ss, kind='cubic') # smoothing the histogram
+		plt.plot(get_inter_bins(bin_edges_max_ss),f_hist_inter(get_inter_bins(bin_edges_max_ss))) 
+		#plt.plot(get_inter_bins(bin_edges_max_ss),hist_max_ss, label='total')
+		plt.legend()
+		plt.show()
+ 
+		
+	return(bin_axis_ss,hist_max_ss/normalisation, hist_max_up/normalisation, hist_max_down/normalisation)
+	
 
 
-        maximum_sim_rem[a]=sim_distance_40_rem[a][minim:maxim].max()
-    range_min=maximum_sim_rem.min()
-    range_max=maximum_sim_rem.max()
-    
-    
-    
-    
-    #print('number of traces for bg =',len(max_ss_bg),'number of up traces =',len(max_ss_up))
-    if plot_histo == 1:
-        # Plot the histogram 
-        plt.figure()
-        plt.hist(ma,bins=num_bins,label='total', alpha = 0.6,range = (range_min,range_max))
-        plt.hist(max_ss_bg,bins=num_bins ,label='background', alpha = 0.5,range = (range_min,range_max))
-        plt.xlabel("Signal")
-        plt.ylabel("Probability")
-        plt.title('Simulated histogram')
-        print('Parameters for the simulation :')
-        print('T_int = %f us, T_in = %f us, T_out = %f us' %(t_int,t_in,t_out))
-        print('A = %f, std = %f'%(A,std_down))
-        plt.legend()
-        plt.show()
-        #plt.savefig('1histogram.png')
 
-        #Plot the histogram as lines 
-        plt.title('Simulated histogram as lines')
-        plt.plot(get_inter_bins(bin_edges_max_ss),hist_max_ss, label='signal')
-        plt.plot(get_inter_bins(bin_edges_max_ss_bg),hist_max_ss_bg,label ='background')
-        plt.legend()
-        plt.xlabel('Distance(V)')
-        plt.grid()
-        #plt.title('Tint = 2.5us')
-
-    return(bin_axis_ss,hist_max_ss/normalisation
-    
-    
-    
-    
-        # HISTOGRAM WITH SIMULATION
-    maximum_sim_rem=np.zeros(len(sim_distance_40_rem))
 
 
 
